804294b411660571879fb723d5704d9b8000c42c
Fan-iX <61686936+Fan-iX@users.noreply.github.com>
2026-04-27 03:18:43 -0400

    add bulk attribute assignment

--- app/components/AttributeInput.vue
+++ app/components/AttributeInput.vue
@@ -1,20 +1,48 @@
 <script setup>
 defineOptions({ inheritAttrs: false })
+import { ref, watch } from 'vue'
 import Popover from './widget/Popover.vue'
 import WInput from './widget/Input.vue'
 import WSelect from './widget/Select.vue'
 import WSelectInput from './widget/SelectInput.vue'
 import WRangeInput from './widget/RangeInput.vue'
 import WColorPicker from './widget/ColorPicker.vue'
+import { VVTreeNode } from '#base/js/vvtree.js'
 
-const { aes, type, label } = defineProps({
-    aes: String,
-    type: String,
-    label: String,
+const { aes, type, label, node, target } = defineProps({
+    aes: String, type: String, label: String,
+    node: VVTreeNode, target: String
 })
-const model = defineModel()
 
-const emit = defineEmits(['spread', 'gather'])
+function applyToDescendants() {
+    let val = node.attributes[aes]
+    node.allChildren.forEach(n => n.attributes[aes] = val)
+}
+
+function gatherFromDescendants() {
+    node.allNodes.toReversed().filter(n => !n.isTip).forEach(n => {
+        let attr = n.children[0]?.attributes[aes]
+        if (n.children.every(c => c.attributes[aes] === attr))
+            n.attributes[aes] = attr
+    })
+}
+function updateAttributes(value) {
+    if (target == "all nodes") {
+        node.root.allNodes.forEach(n => n.attributes[aes] = value)
+    } else if (target == "tips") {
+        node.root.allTips.forEach(n => n.attributes[aes] = value)
+    } else if (target == "descendant nodes") {
+        node.allNodes.forEach(n => n.attributes[aes] = value)
+    } else if (target == "descendant tips") {
+        node.allTips.forEach(n => n.attributes[aes] = value)
+    } else {
+        node.attributes[aes] = value
+    }
+}
+const model = ref()
+watch([() => node, () => target], () => {
+    model.value = node.attributes[aes]
+}, { immediate: true })
 </script>
 
 <template>
@@ -22,18 +50,25 @@ const emit = defineEmits(['spread', 'gather'])
         <div class="flex-1 min-w-0 flex items-center gap-1">
             <slot>
                 {{ label }}:
-                <WInput type="number" v-if="type == 'number'" v-bind="$attrs" v-model="model" />
-                <WColorPicker v-else-if="type == 'color'" v-bind="$attrs" v-model="model" />
-                <WSelect v-else-if="type == 'select'" v-bind="$attrs" v-model="model" />
-                <WSelectInput v-else-if="type == 'option'" v-bind="$attrs" v-model="model" />
-                <WRangeInput v-else-if="type == 'range'" v-bind="$attrs" v-model="model" />
-                <WInput v-else-if="type == 'text'" v-bind="$attrs" v-model="model" />
-                <Icon icon="lucide:x" @click="model = undefined" class="hover:text-red-500 cursor-pointer shrink-0" />
+                <WInput type="number" v-if="type == 'number'" v-bind="$attrs" v-model="model"
+                    @update:model-value="updateAttributes" />
+                <WColorPicker v-else-if="type == 'color'" v-bind="$attrs" v-model="model"
+                    @update:model-value="updateAttributes" />
+                <WSelect v-else-if="type == 'select'" v-bind="$attrs" v-model="model"
+                    @update:model-value="updateAttributes" />
+                <WSelectInput v-else-if="type == 'option'" v-bind="$attrs" v-model="model"
+                    @update:model-value="updateAttributes" />
+                <WRangeInput v-else-if="type == 'range'" v-bind="$attrs" v-model="model"
+                    @update:model-value="updateAttributes" />
+                <WInput v-else-if="type == 'text'" v-bind="$attrs" v-model="model"
+                    @update:model-value="updateAttributes" />
+                <Icon icon="lucide:x" @click="updateAttributes(model = undefined)"
+                    class="hover:text-red-500 cursor-pointer shrink-0" />
             </slot>
         </div>
         <Popover mode="hover" variant="tooltip" side="left">
             <template #trigger>
-                <button class="align-middle rounded-md px-1 py-1 hover:bg-current/5" @click="emit('spread', aes)">
+                <button class="align-middle rounded-md px-1 py-1 hover:bg-current/5" @click="applyToDescendants">
                     <Icon icon="lucide:git-fork" class="rotate-90" />
                 </button>
             </template>
@@ -41,7 +76,7 @@ const emit = defineEmits(['spread', 'gather'])
         </Popover>
         <Popover mode="hover" variant="tooltip" side="left">
             <template #trigger>
-                <button class="align-middle rounded-md px-1 py-1 hover:bg-current/5" @click="emit('gather', aes)">
+                <button class="align-middle rounded-md px-1 py-1 hover:bg-current/5" @click="gatherFromDescendants">
                     <Icon icon="lucide:git-fork" class="rotate-270" />
                 </button>
             </template>
